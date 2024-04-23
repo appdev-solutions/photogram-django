1a24dcdf60d3bdef4d8a015ed972f585c3cb7200
Ben Purinton <ben.purinton@gmail.com>
2024-04-23 22:19:43 +0000

    add photo model, migrate, add to admin dash, manually add some photos

--- photogram/admin.py
+++ photogram/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Photo
 
-# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = [
+        "pk",
+        "image",
+        "owner",
+        "created_at",
+        "updated_at",
+    ]
+
+admin.site.register(Photo, PhotoAdmin)
--- photogram/models.py
+++ photogram/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Photo(models.Model):
+    image = models.CharField(max_length=200)
+    comments_count = models.IntegerField(default=0)
+    likes_count = models.IntegerField(default=0)
+    caption = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.image
