1f22c9ebe65f013be63c29c499a981f3a25c0024
Ben Purinton <ben.purinton@gmail.com>
2024-04-23 23:06:03 +0000

    option 2 to enforce login: custom Middleware

--- photogram/views.py
+++ photogram/views.py
@@ -1,7 +1,6 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Photo
 
-class PhotoListView(LoginRequiredMixin, ListView):
+class PhotoListView(ListView):
     model = Photo
     template_name = "photos/photo_list.html"
