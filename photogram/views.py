5056e0fea677f25b8dfd4e6b41d1045956a0c302
Ben Purinton <ben.purinton@gmail.com>
2024-04-23 22:55:14 +0000

    option 1 to enforce login: MixIn in views

--- photogram/views.py
+++ photogram/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from .models import Photo
 
-class PhotoListView(ListView):
+class PhotoListView(LoginRequiredMixin, ListView):
     model = Photo
     template_name = "photos/photo_list.html"
