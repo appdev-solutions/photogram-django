47f14ce8d4b55ecc893a399d6dfff1e450d3a449
Ben Purinton <ben.purinton@gmail.com>
2024-04-23 23:51:04 +0000

    add Like, Comment, FollowRequest models; migrate

--- photogram/models.py
+++ photogram/models.py
@@ -10,3 +10,28 @@ class Photo(models.Model):
 
     def __str__(self):
         return self.image
+
+class Like(models.Model):
+    fan = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FollowRequest(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("accepted", "Accepted"),
+        ("rejected", "Rejected"),
+    )
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="received_follow_requests")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sent_follow_requests")
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
