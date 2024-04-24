9a392f6b3cc3a5babb261fb1d9dffb96eaeaebd9
Ben Purinton <ben.purinton@gmail.com>
2024-04-24 21:54:43 +0000

    new photo: form, view, url, template, nav link

--- photogram/urls.py
+++ photogram/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PhotoListView, UserDetailView
+from .views import PhotoListView, UserDetailView, PhotoCreateView
 
 urlpatterns = [
     path("photos/", PhotoListView.as_view(), name="photos"),
+    path("photos/new", PhotoCreateView.as_view(), name="photo_create"),
     path("users/<str:username>/", UserDetailView.as_view(), name="user_detail"),
 ]
--- photogram/views.py
+++ photogram/views.py
@@ -1,10 +1,24 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse
 from .models import Photo
 from accounts.models import User
+from .forms import PhotoForm
 
 class PhotoListView(ListView):
     model = Photo
     template_name = "photos/photo_list.html"
+
+class PhotoCreateView(CreateView):
+    model = Photo
+    form_class = PhotoForm
+    template_name = "photos/photo_form.html"
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("user_detail", kwargs={"username": self.request.user.username})
 
 class UserDetailView(DetailView):
     model = User
