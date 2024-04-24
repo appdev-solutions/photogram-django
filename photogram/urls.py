4dee6e5603f2bb321478db85fbb290b849979b59
Ben Purinton <ben.purinton@gmail.com>
2024-04-24 19:43:06 +0000

    user profile page: View, URL, Template

--- photogram/urls.py
+++ photogram/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PhotoListView
+from .views import PhotoListView, UserDetailView
 
 urlpatterns = [
     path("photos/", PhotoListView.as_view(), name="photos"),
+    path("users/<str:username>/", UserDetailView.as_view(), name="user_detail"),
 ]
--- photogram/views.py
+++ photogram/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Photo
+from accounts.models import User
 
 class PhotoListView(ListView):
     model = Photo
     template_name = "photos/photo_list.html"
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = "users/user_detail.html"
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
