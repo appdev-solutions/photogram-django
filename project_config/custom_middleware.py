50da67652d5ef999253aa615564b87530b79bf47
Ben Purinton <ben.purinton@gmail.com>
2024-04-24 00:48:13 +0000

    unblock admin route from custom middleware

--- project_config/custom_middleware.py
+++ project_config/custom_middleware.py
@@ -7,6 +7,6 @@ class LoginRequiredMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        if not request.user.is_authenticated and not any(path in request.path for path in ["login", "logout", "signup"]):
+        if not request.user.is_authenticated and not any(path in request.path for path in ["login", "logout", "signup", "admin"]):
             return redirect(reverse("login"))
         return response
