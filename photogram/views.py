8d6a67847f13aad0888785e215dd680c2a01c9f7
Ben Purinton <ben.purinton@gmail.com>
2024-04-25 03:49:59 +0000

    refactor user detail page with class-based views to handle get versus comment post

--- photogram/views.py
+++ photogram/views.py
@@ -2,6 +2,9 @@ from django.views.generic import (
     ListView, DetailView, CreateView, UpdateView
 )
 from django.urls import reverse
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import FormView
+from django.views import View
 from .models import Photo
 from accounts.models import User
 from .forms import PhotoForm, CommentForm
@@ -34,7 +37,7 @@ class PhotoCreateView(CreateView):
     def get_success_url(self):
         return reverse("user_detail", kwargs={"username": self.request.user.username})
 
-class UserDetailView(DetailView):
+class UserDetailGet(DetailView):
     model = User
     template_name = "users/user_detail.html"
     context_object_name = "user"
@@ -46,11 +49,32 @@ class UserDetailView(DetailView):
         context["form"] = CommentForm()
         return context
 
+class UserDetailPost(SingleObjectMixin, FormView):
+    model = User
+    form_class = CommentForm
+    template_name = "users/user_detail.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
     def post(self, request, *args, **kwargs):
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.photo = Photo.objects.get(id=request.POST.get("photo_id"))
-            comment.save()
-        return super().get(request, *args, **kwargs)
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.author = self.request.user
+        comment.photo = Photo.objects.get(id=self.request.POST.get("photo_id"))
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("user_detail", kwargs={"username": self.object.username})
+
+class UserDetailView(View):
+    def get(self, request, *args, **kwargs):
+        view = UserDetailGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = UserDetailPost.as_view()
+        return view(request, *args, **kwargs)
